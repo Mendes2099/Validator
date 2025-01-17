122c6f5c818c06215976c69258f0a05a91b0cee2
João Mendes <mendes19966@gmail.com>
2025-01-17 13:23:11 +0000

    Begun backend remake due to issue

--- sscc-validator/Back/sscc-validator/src/main/java/com/example/sscc_validator/repository/SSCCRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sscc_validator.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sscc_validator.model.SSCCRequest;
-
-public interface SSCCRequestRepository extends JpaRepository<SSCCRequest, Long> {}
